715923f2088434678f30b5573d430accd9cfb49b
Jack Harner <jackharner3@gmail.com>
2023-06-14 10:29:33 -0400

    small edits before 6.64

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,6 +5,17 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is: ${val}`);
+    if (req.params.id * 1 > tours.length || req.params.id * 1 < 0) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    next();
+}
+
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
 
@@ -21,15 +32,6 @@ exports.getTour = (req, res) => {
     console.log(req.params);
     const id = req.params.id * 1; // convert string to number
     const tour = tours.find(el => el.id === id);
-
-    // if (id > tours.length) {
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid id'
-        });
-    }
-
 
     res.status(200).json({
         status: 'success',
@@ -63,13 +65,6 @@ exports.updateTour = (req, res) => {
     const id = req.params.id * 1; // convert string to number
     let tour = tours.find(el => el.id === id);
 
-    if (id > tours.length || id < 0) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid id'
-        });
-    }
-
     tour = req.body;
     tours.splice(id, 1, tour)
 
@@ -84,12 +79,7 @@ exports.updateTour = (req, res) => {
 }
 
 exports.deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
+
 
     res.status(204).json({
         status: 'success',
