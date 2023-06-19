11b1309b22583ffde1a1821e6f995c2906f6006b
Jack Harner <jackharner3@gmail.com>
2023-06-19 10:05:14 -0400

    before starting mongoose

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const mongoose = require('mongoose');
 const dontenv = require('dotenv');
 const app = require('./app');
 
 dontenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
 
+mongoose.connect(DB, {
+    useNewUrlParse: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {console.log('DB connection successful!');})
+.catch(() => console.log('DB CONNECTION UNSUCCESSFUL'));
 
 // console.log(process.env);
 
