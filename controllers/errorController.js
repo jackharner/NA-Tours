41a1cc2c54e0a7ae1452b85e523a9bdf6e6cfad9
Jack Harner <jackharner3@gmail.com>
2023-06-25 15:32:20 -0400

    end of section 9-123

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,73 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+}
+
+const handleDuplicateFieldsDB = err => {
+
+    const value = Object.values(err.keyValue)[0]; // from course comment, not from jonas
+    const message = `Duplicate field value: ${value}. Please use another value!`
+    return new AppError(message, 400);
+}
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message)
+
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+}
+
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+}
+
+const sendErrorProd = (err, res) => {
+    // Operational, trusted error: send message to client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else { // Programming or other unknown error: don't leak error details
+        // 1) Log error
+        console.error('Error', err);
+
+        // 2) Send generic message
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!'
+        });
+    }
+};
+
 module.exports = (err, req, res, next) => { // this is error handling middleware only called when an error occurs (specifying 4 paramaters tells express that this is error handling middleware)
     // console.log(err.stack);
     
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+    if(process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+        console.debug(err);
+
+    } else if (process.env.NODE_ENV === 'production') {
+        let error = JSON.parse(JSON.stringify(err));
+
+        // Operational errors that were created by code other than our own (like MongoDB or Mongoose errors)
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        if (error.name === 'ValidationError')
+            error = handleValidationErrorDB(error);
+        
+        sendErrorProd(error, res);
+    }
+
+    
 }
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const dontenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+    console.log('UNHANDLED EXCEPTION! Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1); // note: this makes "[nodemon] app crashed - waiting for file changes before starting..." message
+});
+
 dontenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -13,11 +20,20 @@ mongoose.connect(DB, {
     useNewUrlParse: true,
     useCreateIndex: true,
     useFindAndModify: false
-}).then(() => {console.log('DB connection successful!');})
-.catch(() => console.log('DB CONNECTION UNSUCCESSFUL'));
+}).then(() => {console.log('DB connection successful!');});
+// .catch(() => console.log('DB CONNECTION UNSUCCESSFUL'));
 
 
 const port = process.env.port || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection', err => { // handling unhandled promise rejection
+    console.log('UNHANDLED REJECTION! Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => { // server.close() allows current requests to finish before closing
+        process.exit(1); // note: this makes "[nodemon] app crashed - waiting for file changes before starting..." message
+    })
+});
+
