4aca24dde92d541eafb3d55be050683c4054b13f
Jack Harner <jackharner3@gmail.com>
2023-07-01 16:02:20 -0400

    savepoint end of section 10

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan'); // logging functionality, 3rd party middleware example
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController')
@@ -8,16 +13,47 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
-if (process.env.NODE_ENV === 'development') {
+// 1) GLOBAL MIDDLEWARES
+app.use(helmet()); // Set security HTTP headers
+
+if (process.env.NODE_ENV === 'development') { // Development logging
     app.use(morgan('dev'));
 }
 
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!' 
+});
+app.use('/api', limiter);
 
-app.use(express.json()); // middleware that allows us to access the body of the request (it's not present without this line)
-app.use(express.static(`${__dirname}/public`)); // middleware that gives us access to static files like HTML files
 
-app.use((req, res, next) => {
+app.use(express.json({ limit: '10kb' })); // middleware that allows us to access the body of the request (it's not present without this line); body parser, reads data from body into req.body
+
+// Data sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against xss attacks
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+    hpp({
+        whitelist: [
+            'duration',
+            'ratingsQuantity',
+            'ratingsAverage',
+            'maxGroupSize',
+            'difficulty',
+            'price'
+        ]
+    })
+);
+
+app.use(express.static(`${__dirname}/public`)); // middleware that allows us to serve static files like HTML files
+
+app.use((req, res, next) => { // Test middleware
     req.requestTime = new Date().toISOString();
     // console.log(req.headers);
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -15,9 +15,15 @@ const signToken = id => {
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
     
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true // not accessed by browser
+    }
+    if(process.env.Node_ENV === 'production') cookieOptions.secure = true; // only sent over secure connection like https
+    res.cookie('jwt', token, cookieOptions);
+
     const clonedUser = JSON.parse(JSON.stringify(user));
     delete clonedUser.password; // don't send password back to client
-
     res.status(statusCode).json({
         status: 'success',
         token,
