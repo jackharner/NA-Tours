7b8adc04e66258b0966f711e5fe2c10f4f0f7d2c
Jack Harner <jackharner3@gmail.com>
2023-07-02 17:47:18 -0400

    savepoint after 11-157

--- models/tourModel.js
+++ models/tourModel.js
@@ -119,6 +119,13 @@ tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7;
 });
 
+// Virtual populate
+tourSchema.virtual('reviews', {
+    ref: 'Review',
+    foreignField: 'tour',
+    localField: '_id'
+})
+
 // DOCUMENT MIDDLEWARE: runs before .save() and .create() but not insertMany, findByIdAndUpdate, or findOneAndUpdate...
 tourSchema.pre('save', function(next) {
     this.slug = slugify(this.name, { lower: true }) // this is the currently document being saved
