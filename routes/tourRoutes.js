6efd2c3288464ac72ddc0c311d284caf9b735aa1
Jack Harner <jackharner3@gmail.com>
2023-07-05 13:37:08 -0400

    end of section 11 savepoint

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -22,6 +22,14 @@ router
     );
 
 router
+    .route('/tours-within/:distance/center/:latlng/unit/:unit')
+    .get(tourController.getToursWithin); // this is a standard format which is an alternate to query strings
+// /tours-within?=233&center=-40,40&unit=mi
+// /tours-within/233/center/-40,45/unit/mi
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
+router
     .route('/')
     .get(tourController.getAllTours)
     .post(authController.protect, authController.restrictTo('admin', 'lead-guide'),tourController.createTour);
