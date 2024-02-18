0823bcc3c91da9251db26d118468b91fc930060e
Jack Harner <jackharner3@gmail.com>
2024-02-18 13:52:55 -0500

    savepoint after 12-183

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -68,13 +69,9 @@ app.use((req, res, next) => { // Test middleware
 
 // 3) ROUTES
 
-app.get('/', (req, res) => {
-    res.status(200).render('base', { // {} object available in pug template
-        tour: 'The Forest Hiker',
-        user: 'Jack'
-    });
-})
 
+
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
