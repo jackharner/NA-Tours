fc63b6ce1324d90c1e79f39e468268da8274a15d
Jack Harner <jackharner3@gmail.com>
2024-02-19 19:23:53 -0500

    savepoint after 12-187

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,12 +14,22 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
     // 1) get the data, for the requested tour (including reviews and guides)
-    // create route
-    // create controller
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user'
+    })
 
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker Tour'
-    });
-};
+
+    res
+        .status(200)
+        .set(
+            'Content-Security-Policy',
+            "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+        )
+        .render('tour', {
+            title: `${tour.name} Tour`,
+            tour,
+        });
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,8 +4,8 @@ const viewsController = require('../controllers/viewsController')
 const router = express.Router();
 
 router.get('/', viewsController.getOverview);
+router.get('/tour/:slug', viewsController.getTour)
 
-router.get('/tour', viewsController.getTour)
 
 module.exports = router;
 
