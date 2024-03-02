56b0038f566a0ba08a44a68d8fee71f1498b1c43
Jack Harner <jackharner3@gmail.com>
2024-03-02 09:32:58 -0500

    savepoint after 13-201

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,34 @@
+const multer = require('multer');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
+
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/img/users');
+    },
+    filename: (req, file, cb) => {
+        // user-767676abs76dba-3432423432.jpeg
+        const ext = file.mimetype.split('/')[1];
+        cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+    }
+});
+
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true)
+    } else {
+        cb(new AppError('Not an image! Please upload only images.', 400), false)
+    }
+}
+
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter
+});
+
+exports.uploadUserPhoto = upload.single('photo');
 
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
@@ -17,8 +44,6 @@ exports.getMe = (req, res, next) => { //middleware to allow getUser to have req.
 }
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-    // console.log(req.file);
-    // console.log(req.body);
 
     // 1) Create error if user POSTs password data
     if (req.body.password || req.body.passwordConfirm) {
@@ -27,6 +52,8 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
     // 2) Filtered out unwanted field names that are not allowed to be updated
     const filteredBody = filterObj(req.body, 'name', 'email');
+    if (req.file) filteredBody.photo = req.file.filename;
+
     // 2) Update user document
     const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
 
--- models/userModel.js
+++ models/userModel.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please tell us your name!']
     },
     email: {
-        type:String,
+        type: String,
         required: [true, 'Please tell us your email!'],
         unique: true,
         lowercase: true,
@@ -23,6 +23,7 @@ const userSchema = new mongoose.Schema({
     },
     photo: {
         type: String,
+        default: 'default.jpg'
     },
     role: {
         type: String,
@@ -39,7 +40,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please confirm your password'],
         validate: {
-            validator: function(el) {
+            validator: function (el) {
                 return el === this.password; // "this" keyword points to current doc on NEW document, must use "Save", not findOneAndUpdate Will only work with update if you take a complex workaround
             },
             message: 'Passwords are not the same'
@@ -55,33 +56,33 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-userSchema.pre('save', async function(next) {
-    if(!this.isModified('password')) return next(); // only run this function if password was modified
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next(); // only run this function if password was modified
 
     this.password = await bcrypt.hash(this.password, 12); // hash the password with cost of 12
 
     this.passwordConfirm = undefined; // Delete the passwordConfirm field value
 });
 
-userSchema.pre('save', function(next) {
-    if(!this.isModified('password') || this.isNew) return next();
+userSchema.pre('save', function (next) {
+    if (!this.isModified('password') || this.isNew) return next();
 
     this.passwordChangedAt = Date.now() - 1000; // ensure that the token is created after password was changed
     next();
 });
 
-userSchema.pre(/^find/, function(next) {
+userSchema.pre(/^find/, function (next) {
     // this points to the current query
-    this.find({ active: {$ne: false} });
+    this.find({ active: { $ne: false } });
     next();
 })
 
-userSchema.methods.correctPassword = async function(candidatePassword, userPassword) { //instance method available on all user documents
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) { //instance method available on all user documents
     return await bcrypt.compare(candidatePassword, userPassword);
 }
 
-userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
-    if(this.passwordChangedAt) {
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+    if (this.passwordChangedAt) {
         const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
 
         return JWTTimestamp < changedTimestamp;
@@ -91,13 +92,13 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
     return false;
 }
 
-userSchema.methods.createPasswordResetToken = function() {
+userSchema.methods.createPasswordResetToken = function () {
     const resetToken = crypto.randomBytes(32).toString('hex');
 
     this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex'); // encrypt what gets sent to the database so a hacker can't use it
 
-    console.log({resetToken}, this.passwordResetToken);
-    
+    console.log({ resetToken }, this.passwordResetToken);
+
     this.passwordResetExpires = Date.now() + 10 * 60 * 1000 // 10 min in miliseconds
 
     return resetToken;
