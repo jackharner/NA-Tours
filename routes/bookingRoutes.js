55d7c2ff5064efebacab9441bc63e9aac6d3e783
Jack Harner <jackharner3@gmail.com>
2024-03-13 15:11:36 -0400

    savepoint after 13-211, before changing stripe code

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const bookingController = require('./../controllers/reviewController');
+const bookingController = require('./../controllers/bookingController');
 const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-// router.get('/checkout-session/:tourID',
-//     authController.protect,
-//     bookingController.getCheckoutSession
-// );
+
+router.get('/checkout-session/:tourId',
+    authController.protect,
+    bookingController.getCheckoutSession
+);
 
 module.exports = router;
